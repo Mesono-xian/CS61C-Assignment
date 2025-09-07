b9fa40c775f5d5adb41ab3b33404c44fac89f155
Sizhuo Li <SizhuoLi@outlook.com>
2025-09-07 17:06:52 +0800

    proj1 A2

--- Project/proj1/steganography.c
+++ Project/proj1/steganography.c
@@ -22,12 +22,48 @@
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
 	//YOUR CODE HERE
+	Color *color;
+	color = (Color*)malloc(sizeof(Color));
+	int check = image->image[row][col].B;
+	if(check%2)
+	{
+		color->R = 255;
+		color->G = 255;
+		color->B = 255;
+	}
+	else
+	{
+		color->R = 0;
+		color->G = 0;
+		color->B = 0;
+	}
+	return color;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
 	//YOUR CODE HERE
+	Image *new;
+	new = (Image *)malloc(sizeof(Image));
+	new->cols = image->cols;
+	new->rows = image->rows;
+	new->image = (Color**)malloc(new->rows * sizeof(Color*));
+	for(int r=0;r<new->rows;r++)
+	{
+		new->image[r] = (Color*)malloc(new->cols * sizeof(Color));
+	}
+	for(int i=0;i<new->rows;i++)
+	{
+		for(int j=0;j<new->cols;j++)
+		{
+			//the implementation of the function use "malloc", so we need to free it in time.
+			Color *new_pixel = evaluateOnePixel(image,i,j);
+			new->image[i][j] = *new_pixel;
+			free(new_pixel);
+		}
+	}
+	return new;
 }
 
 /*
@@ -46,4 +82,16 @@ Make sure to free all memory before returning!
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if(argc!=2)
+	{
+		exit(-1);
+	}
+	char *filename;
+	filename = argv[1];
+	Image *pre_im = readData(filename);
+	Image *new_im = steganography(pre_im);
+	writeData(new_im);
+	freeImage(new_im);
+	freeImage(pre_im);
+	return 0;
 }
