45f98f80605fc8076f473487dbd1178483f8e8c9
李斯卓 <liqiang@luminadeMacBook-Pro.local>
2025-09-11 20:54:34 +0800

    lab02

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -3,8 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+#define reg(x) get_bit(*reg,x)
+unsigned get_bit(unsigned x,
+                 unsigned n) {
+    // YOUR CODE HERE
+    // Returning -1 is a placeholder (it makes
+    // no sense, because get_bit only returns 
+    // 0 or 1)
+    return (x >> n) & 1;
+}
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    int next = ((reg(0) ^ reg(2)) ^ reg(3)) ^ reg(5);
+    *reg = *reg >> 1;
+    *reg |= (next << 15);
 }
 
